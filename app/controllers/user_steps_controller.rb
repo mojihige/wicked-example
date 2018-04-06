b0deed7d75cab13557d0d1439f7c40dcd5b92200
mojihige <ayatakayuram2605@gmail.com>
2018-04-06 23:25:29 +0900

    user_stepコントローラにwickedの処理を追加

--- app/controllers/user_steps_controller.rb
+++ app/controllers/user_steps_controller.rb
@@ -1,2 +1,25 @@
 class UserStepsController < ApplicationController
+  include Wicked::Wizard
+  steps :second, :third
+
+  def show
+    @user = current_user
+    render_wizard
+  end
+
+  def update
+    @user = current_user
+    @user.attriutes = params[:user]
+    render_wizard @user
+  end
+
+  private
+
+  def finish_wizard_path
+    users_path(current_user)
+  end
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
 end
